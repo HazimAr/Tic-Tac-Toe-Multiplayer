9fb814773ac93a9b1eb0006f3dbd5cef4d264efd
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-09 13:56:15 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-const DB_URL = "https://tic-calpico.herokuapp.com/";
+const DB_URL = "https://tic-calpico.herokuapp.com";
+// const DB_URL = "https://f8513c61268b.ngrok.io";
 
-type MetaType = {
+
+https: type MetaType = {
 	title: string;
 	lang: string;
 	description: string;
